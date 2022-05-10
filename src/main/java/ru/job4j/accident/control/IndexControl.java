fc3f291eab46f442ed23590ca132b9d67c296018
meavv <c.c.c.p@bk.ru>
2022-05-10 13:14:07 +0500

    3. @ModelAttribute. Создание инцидента. [#261013]

--- src/main/java/ru/job4j/accident/control/IndexControl.java
+++ src/main/java/ru/job4j/accident/control/IndexControl.java
@@ -4,10 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.LinkedList;
+import java.util.List;
+
 @Controller
 public class IndexControl {
     @GetMapping("/")
     public String index(Model model) {
+        List<String> list = new LinkedList<>();
+        list.add("Строка1");
+        list.add("Строка2");
+        list.add("Строка3");
+        list.add("Строка4");
+        model.addAttribute("list", list);
+        model.addAttribute("count", 0);
         return "index";
     }
 }
