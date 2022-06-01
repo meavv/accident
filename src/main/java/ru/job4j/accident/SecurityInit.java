939300b5343d1e87c0e3bad7d1d637277d7874d3
meavv <c.c.c.p@bk.ru>
2022-06-01 19:03:33 +0500

    0. Spring Security [#6879 #307810]

--- src/main/java/ru/job4j/accident/SecurityInit.java
+++ src/main/java/ru/job4j/accident/SecurityInit.java
@@ -2,5 +2,5 @@ package ru.job4j.accident;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
-public class SecurityInit {
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer  {
 }
